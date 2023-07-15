59c89e74fd1171f4c22d06741aa237a5920766b8
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-07-15 13:36:27 +0545

    add products data

---
+++ src/products.js
@@ -0,0 +1,25 @@
+export const products = [
+{
+    "p_id":3,
+    "name":"Samsung Galaxy A23 (6GB/128GB) 90Hz Display 50MP Camera 5000mAh Battery",
+    "price":27999,
+    "rating":null,
+    "category":"Electronics"
+},
+{
+    "p_id":2,
+    "name":"Rock On Stereo Headset DV 07",
+    "price":99,
+    "rating":null,
+    "category":"Music"
+},
+{
+    "p_id":1,
+    "name":"Aluminium Alloy Adjustable Laptop Stand For 10 to 17 Inches Laptop/Tab",
+    "price":640,
+    "rating":null,
+    "category":"Electronics"
+}
+];
+
+// export default products;
