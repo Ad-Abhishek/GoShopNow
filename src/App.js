5c726522165fbf6cecd3de17eb47a67f83aec7ea
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-07-15 13:32:11 +0545

    update app and add Routes

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import Header from "./components/Header";
 import LoginScreen from "./components/LoginScreen";
 import RegisterScreen from "./components/RegisterScreen";
 import { Product } from "./components/Product";
+import ProductScreen from "./components/ProductScreen";
+import { Toaster } from "react-hot-toast";
+
 
 function App() {
   return (
     <>
       <Header />
+      <Toaster position="top-right" toastOptions={{duration: 2000}}/>
       <Routes>
         <Route path="/" element={<Product />} />
         <Route path="/register" element={<RegisterScreen />} />
         <Route path="/login" element={<LoginScreen />} />
+        <Route path="/product/:id" element={<ProductScreen />} />
       </Routes>
       
       <Footer />
