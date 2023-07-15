441ee9fa2d88ff3d1add1a8fa2cf02b24504ebf9
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-07-15 17:54:42 +0545

    add Cart screen initial

--- src/screens/CartScreen.jsx
+++ src/screens/CartScreen.jsx
@@ -5,7 +5,7 @@ import {
     Col,
     ListGroup,
     Image,
-    // Form,
+    Form,
     Button,
     Card,
 } from 'react-bootstrap';
@@ -49,22 +49,19 @@ const CartScreen = () => {
                                         <Image src='https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&w=400' alt=' item.name ' fluid rounded />
                                     </Col>
                                     <Col md={3}>
-                                        <Link to='/product/5'>item.name</Link>
+                                        <Link to='/product/2'>item.name</Link>
                                     </Col>
                                     <Col md={2}>
                                         $ item.price 
                                     </Col>
                                     <Col md={2}>
-                                    {/* <Form.Control
-                                                    as='select'
-                                                    value={item.qty}
-                                                    onChange={(e) => addToCartHandler(item, Number(e.target.value))}>
-                                                        {[...Array(item.countInStock).keys()].map((x) => (
-                                                            <option key={x + 1} value={x + 1}>
-                                                                {x + 1}
-                                                            </option>
-                                                        ))}
-                                                </Form.Control> */}
+                                    <Form.Control
+                                        as='select'
+                                    >
+                                        <option >1</option>
+                                        <option >2</option>
+                                        <option >3</option>
+                                    </Form.Control>
                                     </Col>
                                     <Col md={2}>
                                         <Button type='button' variant='light' onClick={''}>
@@ -85,6 +82,7 @@ const CartScreen = () => {
                             Subtotal
                             {/* Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)}) items */}
                         </h2>
+
                         {/* ${ cartItems.reduce((acc, item) => acc + item.qty * item.price, 0).toFixed(2)} */}
                     </ListGroup.Item>
                     <ListGroup.Item>
