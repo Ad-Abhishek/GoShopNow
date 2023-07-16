460824e4f53e2ae0f22cf38da85ac65ea2216989
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-07-16 05:53:08 +0545

    implement login

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,19 +1,49 @@
 import { Form, Button, Row, Col } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
+import { useState } from 'react';
+import axios from 'axios';
+import { toast } from 'react-hot-toast';
 
 const LoginScreen = () => {
+    const navigate = useNavigate();
+
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    });
+
+    const loginUser = async (e) => {
+        e.preventDefault();
+        const { email, password } = data;
+
+        try {
+            const { data } = await axios.post('https://goshopnow.onrender.com/api/user/token/', {
+                email,
+                password
+            })
+            if (data.error) {
+                toast.error("Error! Try again")
+            } else {
+                setData({});
+                toast.success("Login successful");
+            }
+        } catch (error) {
+            console.log(error)
+        }
+    }
     return (
         <FormContainer>
-        
             <h1>Sign In</h1>
 
-            <Form onSubmit={''}>
+            <Form onSubmit={loginUser}>
                 <Form.Group controlId='email' className='my-3'>
                     <Form.Label>Email Address</Form.Label>
                     <Form.Control
                         type='email'
                         placeholder='Enter email'
+                        value={data.email}
+                        onChange={(e) => setData({...data, email : e.target.value})}
                     ></Form.Control>    
                 </Form.Group>
 
@@ -22,6 +52,8 @@ const LoginScreen = () => {
                     <Form.Control
                         type='password'
                         placeholder='Enter password'
+                        value={data.password}
+                        onChange={(e) => setData({...data, password : e.target.value})}
                     ></Form.Control>    
                 </Form.Group>
 
