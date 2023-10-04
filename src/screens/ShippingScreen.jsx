6ec19748e3e57533f1f2bee740de9923bd0c9ed3
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-10-04 17:29:05 +0545

    get user's previous shipping address

--- src/screens/ShippingScreen.jsx
+++ src/screens/ShippingScreen.jsx
@@ -1,11 +1,45 @@
-import { Form, Button } from 'react-bootstrap';
+import { useState } from 'react';
+import axios from "axios";
+import { Form, Button, FormGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
 import { CheckoutSteps } from "../components/CheckoutSteps";
 
 export const ShippingScreen = () => {
     
+    const [ userAddress, setUserAddress ] = useState([]);
+    const [ addresses, setAddresses ] = useState([]);
+
+    const get_previous_address_URL = 'http://127.0.0.1:8000/api/address/delivery-address/';
+
     const navigate = useNavigate();
+
+    let cityName = '';
+    let provinceName = '';
+    let areaName = '';
+
+    const getPreviousAddress = () => {
+        const token = localStorage.getItem('jwt');
+
+        const headers = {
+            'Authorization' : `Bearer ${token}`
+        }
+
+        axios 
+            .get(get_previous_address_URL, 
+                {headers})
+            .then((res) => {
+                setUserAddress(() => {                
+                    cityName = res.data[0].city;
+                    provinceName = res.data[0].provience;
+                    areaName = res.data[0].area;
+                    addresses.push(provinceName, cityName, areaName)
+            }); 
+            })
+            .catch((err) => {
+                console.log(err);
+            });  
+    }
 
     const submitHandler = (e) => {
         e.preventDefault();
@@ -13,45 +47,56 @@ export const ShippingScreen = () => {
     };
 
     return (
+        <>  
         <FormContainer>
             <CheckoutSteps step1 step2 />
 
             <h1>Shipping</h1>
+                <Form  >
+                    <Form.Group controlId="province" className="my-2" >
+                        <Form.Label>Province</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter province"
+                            value={addresses[0]}
+                        ></Form.Control>
+                    </Form.Group>
 
-            <Form onSubmit={submitHandler}>
-                <Form.Group controlId="province" className="my-2">
-                    <Form.Label>Province</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter province"
-                    ></Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="city" className="my-2">
+                        <Form.Label>City</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter city"
+                            value={addresses[1]}
+                        ></Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="city" className="my-2">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter city"
-                    ></Form.Control>
-                </Form.Group>
+                    <Form.Group controlId="area" className="my-2">
+                        <Form.Label>Area</Form.Label>
+                        <Form.Control
+                            type="text"
+                            placeholder="Enter area"
+                            value={addresses[2]}
+                        ></Form.Control>
+                    </Form.Group>
 
-                <Form.Group controlId="area" className="my-2">
-                    <Form.Label>Area</Form.Label>
-                    <Form.Control
-                        type="text"
-                        placeholder="Enter area"
-                    ></Form.Control>
-                </Form.Group>
-                
-                <Button
-                    type="submit"
-                    variant="primary"
-                    className="my-2"
-                >
-                    Continue
-                </Button>
-            </Form>
+                    <FormGroup>
+                        <Form.Label onClick={getPreviousAddress}>
+                           click to get previous address
+                        </Form.Label>
+                    </FormGroup>
+                    
+                    <Button
+                        type="submit"
+                        variant="primary"
+                        className="my-2"
+                        onClick={submitHandler}
+                    >
+                        Continue
+                    </Button>
+                </Form>  
         </FormContainer>
+    </>
     )
 }
 
