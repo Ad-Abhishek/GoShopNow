3a939bc24d4f82c15e34165c67410c73add50547
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-10-05 08:31:49 +0545

    dynamic dropdown for delivery address

--- src/screens/ShippingScreen.jsx
+++ src/screens/ShippingScreen.jsx
@@ -1,96 +1,194 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import axios from "axios";
 import { Form, Button, FormGroup } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import FormContainer from '../components/FormContainer';
 import { CheckoutSteps } from "../components/CheckoutSteps";
+import { toast } from 'react-hot-toast'; 
 
 export const ShippingScreen = () => {
     
-    const [ userAddress, setUserAddress ] = useState([]);
-    const [ addresses, setAddresses ] = useState([]);
+    const [ address, setAddress ] = useState([]);
+    const [ city, setCity ] = useState([]);
+    const [ area, setArea ] = useState([]);
+    const [data,setData] = useState({
+        provience:'',
+        city:'',
+        area:''
+    });
 
-    const get_previous_address_URL = 'http://127.0.0.1:8000/api/address/delivery-address/';
+    const address_URL = 'http://127.0.0.1:8000/api/address/address';
+    const post_address_URL = 'http://127.0.0.1:8000/api/address/delivery-address/';
 
     const navigate = useNavigate();
 
-    let cityName = '';
-    let provinceName = '';
-    let areaName = '';
+    const token = localStorage.getItem('jwt');
 
-    const getPreviousAddress = () => {
-        const token = localStorage.getItem('jwt');
+    const headers = {
+        'Authorization' : `Bearer ${token}`
+    }
 
-        const headers = {
-            'Authorization' : `Bearer ${token}`
-        }
+    useEffect(() => {
+        getAddress();
+    }, [address_URL])
 
+    const getAddress = () => {
+        
         axios 
-            .get(get_previous_address_URL, 
+            .get(address_URL, 
                 {headers})
             .then((res) => {
-                setUserAddress(() => {                
-                    cityName = res.data[0].city;
-                    provinceName = res.data[0].provience;
-                    areaName = res.data[0].area;
-                    addresses.push(provinceName, cityName, areaName)
-            }); 
-            })
+                setAddress(() => {
+                    return res.data;
+                })
+            })   
             .catch((err) => {
                 console.log(err);
             });  
     }
 
+    const getCity = (e) => {
+        let url = `${address_URL}?parent=${e.target.value}`;
+
+        axios
+            .get(url, {headers})
+            .then((res) => {
+                setCity(() => {
+                    return res.data
+                })
+                setData(()=>{
+                    return {
+                        ...data,
+                        provience: e.target.value
+                    }
+                })
+            })
+
+            .catch((err) => {
+                console.log(err.data)
+            })
+        
+    }
+
+    const getArea = (e) => {
+        let url = `${address_URL}?parent=${e.target.value}`;
+
+        axios
+            .get(url, {headers})
+            .then((res) => {
+                setArea(() => {
+                    return res.data
+                })
+                setData(()=>{
+                    return {
+                        ...data,
+                        city: e.target.value
+                    }
+                })
+            })
+            
+            .catch((err) => {
+                console.log(err.data)
+            })
+    }
+
+    const handleChangeArea = (e)=>{
+        setData(()=>{
+            return {
+                ...data,
+                area: e.target.value
+            }
+        })
+
+    }
     const submitHandler = (e) => {
         e.preventDefault();
-        navigate('/payment');
+
+        axios
+            .post(post_address_URL, data, {headers})
+            .then((res) => {
+                if (res.status === 201) {
+                    toast.success("Delivery address added!")
+                    navigate('/payment');
+                }
+            }
+            )
+            .catch(()=>{
+                toast.error("Add Delivery address failed!")
+            })    
     };
 
     return (
         <>  
-        <FormContainer>
+        <FormContainer >
             <CheckoutSteps step1 step2 />
-
             <h1>Shipping</h1>
                 <Form  >
-                    <Form.Group controlId="province" className="my-2" >
-                        <Form.Label>Province</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter province"
-                            value={addresses[0]}
-                        ></Form.Control>
-                    </Form.Group>
-
-                    <Form.Group controlId="city" className="my-2">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter city"
-                            value={addresses[1]}
-                        ></Form.Control>
-                    </Form.Group>
-
-                    <Form.Group controlId="area" className="my-2">
-                        <Form.Label>Area</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Enter area"
-                            value={addresses[2]}
-                        ></Form.Control>
-                    </Form.Group>
-
                     <FormGroup>
-                        <Form.Label onClick={getPreviousAddress}>
-                           click to get previous address
-                        </Form.Label>
-                    </FormGroup>
+                        <label>Province:</label>
+
+                        <select 
+                                defaultValue={'title'} 
+                                className="form-select" 
+                                aria-label="Default select example"
+                                onChange={(e)=>{getCity(e)}}
+                                >
+                            <option value='title'>--Select--</option>
+                            {
+                                address.map((addr) => {
+                                    return (
+                                        <option value={addr.id} key={addr.id}>{addr.name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </FormGroup> 
+
+                    {city && <FormGroup>
+                        <label>City:</label>
+
+                        <select 
+                                defaultValue={'title'} 
+                                className="form-select" 
+                                aria-label="Default select example"
+                                onChange={(e)=>{getArea(e)}}
+                                >
+                            <option value='title'>--Select--</option>
+                            {
+                                city.map((cit) => {
+                                    return (
+                                        <option value={cit.id} key={cit.id}>{cit.name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </FormGroup>}
+
+                    {area && <FormGroup>
+                        <label>Area:</label>
+
+                        <select 
+                                defaultValue={'title'} 
+                                className="form-select" 
+                                aria-label="Default select example"
+                                onChange={(e)=>{handleChangeArea(e)}}
+                                >
+                            <option value='title' >--Select--</option>
+                            {
+                                area.map((are) => {
+                                    return (
+                                        <option value={are.id} key={are.id}>{are.name}</option>
+                                    )
+                                })
+                            }
+                        </select>
+                    </FormGroup>}
                     
                     <Button
                         type="submit"
                         variant="primary"
                         className="my-2"
-                        onClick={submitHandler}
+                        onClick={(e) => submitHandler(e)}
                     >
                         Continue
                     </Button>
