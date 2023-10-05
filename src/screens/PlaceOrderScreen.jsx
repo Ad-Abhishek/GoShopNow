fae510de1ff066c94eff53939531d3de81539859
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-10-05 13:26:12 +0545

    verify purchase with url params

--- src/screens/PlaceOrderScreen.jsx
+++ src/screens/PlaceOrderScreen.jsx
@@ -1,122 +1,50 @@
-import { Link } from 'react-router-dom';
-import { Card, Button, Row, Col, ListGroup, Image} from 'react-bootstrap';
-import CheckoutSteps from "../components/CheckoutSteps";
-import Message from '../components/Message';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import {  useSearchParams  } from 'react-router-dom';
 
 const PlaceOrderScreen = () => {
 
+    const [data, setData] = useState()
+    const [searchParams] = useSearchParams();
+
+    const token = localStorage.getItem('jwt');
+
+    const headers = {
+        'Authorization' : `Bearer ${token}`
+    }
+
+    useEffect(()=>{
+        const params = {}
+        for(const [key,value] of searchParams){
+            params[key] = value
+        }
+        setData(params)
+   },[searchParams])
+
+   function verifyPurchase(){
+        // let url =  `http://127.0.0.1:8000/api/payment/validate/?amount=28000&mobile=98XXXXX001&pidx=uDNqXPxv7GyN6mVi8GMTnP&purchase_order_id=ceaca50d-9fdc-4629-9019-10594fc858f3&purchase_order_name=ceaca50d-9fdc-4629-9019-10594fc858f3&transaction_id=GTfRkSSN55WAq4YLvogAyJ&txnId=Yp6c5dVwWfXszzk5YfeqiJ`;
+        let url = `http://127.0.0.1:8000/api/payment/validate/?pidx=${data.pidx}&txnId=${data.txnId}&amount=${data.amount}&mobile=${data.mobile}&purchase_order_id=${data.purchase_order_id}&purchase_order_name=${data.purchase_order_name}&transaction_id=${data.transaction_id}`;
+        
+        console.log("payment verification URL" + url);
+        axios
+            .get(url,{headers})
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+   }
+
   return (
     <>
-        <CheckoutSteps step1 step2 step3 step4 />
-        <Row>
-            <Col md={8}>
-                <ListGroup variant="flush">
-                    <ListGroup.Item>
-                        <h2>Shipping</h2>
-                        <p>
-                            <strong>Address:</strong>
-                            pool, Pokhara{' '}
-                            1716,{' '}
-                            Nepal
-                        </p>
-                    </ListGroup.Item>
-
-                    <ListGroup.Item>
-                        <h2>Payment Mehod</h2>
-                        <strong>Method: </strong>
-                        Khalti
-                    </ListGroup.Item>
-
-                    <ListGroup.Item>
-                        <h2>Order Items</h2>
-                        {/* {cart.cartItems.length === 0 ? ( */}
-                            <Message>Your cart is empty</Message>
-                        {/* ) : ( */}
-                            <ListGroup variant="flush">
-                                {/* { cart.cartItems.map((item, index) => ( */}
-                                    <ListGroup.Item key='1'>
-                                        <Row>
-                                            <Col md={1}>
-                                                <Image
-                                                    src='https://images.pexels.com/photos/2607544/pexels-photo-2607544.jpeg?auto=compress&cs=tinysrgb&w=400'
-                                                    alt='item.name'
-                                                    fluid
-                                                    rounded
-                                                />    
-                                            </Col>
-
-                                            <Col>
-                                                <Link to={`/product/2`}>
-                                                    item.name
-                                                </Link>
-                                            </Col>
-
-                                            <Col md={4}>
-                                                1 * 45 = 45
-                                                {/* { item.qty } x ${ item.price } = ${ item.qty * item.price } */}
-                                            </Col>
-                                        </Row>
-                                    </ListGroup.Item>
-                                {/* ))} */}
-                            </ListGroup>
-                        {/* )} */}
-                    </ListGroup.Item>
-                </ListGroup>
-            </Col>
-            <Col md={4}>
-                <Card>
-                    <ListGroup variant="flush">
-                        <ListGroup.Item>
-                            <h2>Order Summary</h2>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Row>
-                               <Col>Items:</Col>
-                               <Col>cart.itemsPrice</Col> 
-                            </Row>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Row>
-                               <Col>Shipping:</Col>
-                               <Col>cart.shippingPrice</Col> 
-                            </Row>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Row>
-                               <Col>Tax:</Col>
-                               <Col>cart.taxPrice</Col> 
-                            </Row>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Row>
-                               <Col>Total:</Col>
-                               <Col>cart.totalPrice</Col> 
-                            </Row>
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            {/* {error && <Message variant='danger'>{error}</Message>} */}
-                        </ListGroup.Item>
-
-                        <ListGroup.Item>
-                            <Button
-                                type="button"
-                                className="btn-block"
-                                // disabled={cart.cartItems.length === 0}
-                                // onClick={ placeOrderHandler }
-                            >
-                                Place Order
-                            </Button>  
-                            {/* { isLoading && <Loader />}   */}
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Card>
-            </Col>
-        </Row>
-    </>
+       <Button
+           onClick={()=>{verifyPurchase()}}
+        >
+            Check Payment Status
+        </Button>
+    </>  
   )
 }
 
