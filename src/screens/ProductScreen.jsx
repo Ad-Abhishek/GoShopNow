ddf9a569e9ca89f8d1ee2375f864d2d39ffa27ae
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2023-10-08 03:40:28 +0545

    product description

--- src/screens/ProductScreen.jsx
+++ src/screens/ProductScreen.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { useParams, Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import { toast } from 'react-hot-toast';
-
 import {
     Form,
     Row,
@@ -24,6 +23,7 @@ const ProductScreen = () => {
     const add_to_cart_URL =`http://127.0.0.1:8000/api/cart/cart/`; 
     let origin = 'http://127.0.0.1:8000';
     const [product, setProduct] = useState({});
+    const [ description, setDescription ] = useState('Turn heads with luscious, jet-black tresses using our black hair color shampoo, your go-to for salon-quality results at home.');
 
     useEffect(() => {
       axios
@@ -78,10 +78,40 @@ const ProductScreen = () => {
             return e.target.value   
         })    
     }
+
+    const displayDescription = (id) => {
+        if (id === 1) {
+            setDescription('awesome product')
+        }
+        if(id === 2) {
+            setDescription('new product')
+        }
+        if (id === 3) {
+            setDescription('Experience the thrill of flight with our helicopter remote toy, designed for kids of all ages to soar through the skies with ease and excitement!')
+        }if(id === 4) {
+            setDescription('Elevate your hair care routine with our revitalizing shampoo, formulated to cleanse, nourish, and leave your locks looking and feeling their best')
+        }
+        if (id === 5) {
+            setDescription("Unleash the excitement with our racing car for kids – the ultimate adrenaline-packed adventure for young speed enthusiasts!")
+        }if(id === 6) {
+            setDescription('Transform your look effortlessly with our black hair color shampoo, delivering vibrant and long-lasting results in just one wash')
+        }
+        if (id === 7) {
+            setDescription("Step into a world of deep, rich black hues with our color-enhancing shampoo – your path to hair excellence")
+        }if(id === 8) {
+            setDescription('Turn heads with luscious, jet-black tresses using our black hair color shampoo, your go-to for salon-quality results at home.')
+        }
+        if (id === 9) {
+            setDescription("Fuel your child's imagination and let them race into adventure with our thrilling racing car for kids – where dreams become reality")
+        }
+        else{ 
+            setDescription('newest in the market')
+        }
+    }
+
+
   return (
     <>
-        {/* {Object.keys(product).length > 0 && JSON.stringify(product.image_url[0].image_url)} */}
-
             <Link className='btn btn-light my-3' to='/'>
                 Go Back
             </Link>
@@ -102,7 +132,7 @@ const ProductScreen = () => {
                         </ListGroupItem>
                         <ListGroup.Item as='h5'>Price: Rs. {product.price}</ListGroup.Item>
                         <ListGroup.Item>Category: {product.category}</ListGroup.Item>
-                        <ListGroup.Item>Description: {product.description} </ListGroup.Item>
+                        <ListGroup.Item >Description: {description} </ListGroup.Item>
                     </ListGroup>
                 </Col>
                 <Col md={3}>
